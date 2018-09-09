35d4b5fd8bca76a8a8a4be7906c968b1a8bc5b65
ccbmarcowang <490795201@qq.com>
2018-09-09 14:48:35 +0800

    finnal

--- excel_matplotlib/ex2img_1.py
+++ excel_matplotlib/ex2img_1.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 # -*- coding:utf-8 -*-
 import matplotlib.pyplot as plt
+from pylab import *
 import xlrd
 from xlrd import open_workbook
 x_data=[]
@@ -19,9 +20,16 @@ for s in wb.sheets():
         x_data.append(values[0])
         y_data.append(values[1])
 plt.plot(x_data,y_data,'bo-',label=u"Phase curve",linewidth=1)
+plt.annotate('zero point',xy=(180,0),xytext=(60,3),arrowprops=dict(facecolor='black',shrink=0.05),)
 plt.title(u'Tr14 phase detector')
 plt.legend()
+ax=gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data',0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data',0))
 plt.xlabel(u'input-deg')
 plt.ylabel(u'output-V')
 plt.show()
-_
